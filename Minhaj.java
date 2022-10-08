71132b6568c7d69f3fdadfb0b4499c1bd1bf572d
syedmuzammil797 <syedmuzammilmm3@gmail.com>
2022-10-07 21:33:06 -0400

    New Commit

---
+++ Minhaj.java
@@ -0,0 +1,7 @@
+public class Minhaj {
+    public static void main (String[] args){
+        System.out.println("Hello Minhaj");
+        System.out.println("Hi faiz");
+        System.out.println("hey did you got new project link");
+    }
+}
