b445e6c3ad8dfd748ebc59a25babf6311c8c20ff
syedmuzammil797 <syedmuzammilmm3@gmail.com>
2022-10-07 21:54:47 -0400

    Conflict

--- Minhaj.java
+++ Minhaj.java
@@ -1,6 +1,6 @@
 public class Minhaj {
     public static void main (String[] args){
-        System.out.println("Hello Minhaj");
+        System.out.println("Hello Faiz");
         System.out.println("Hi faiz");
         System.out.println("hey did you got new project link");
         //NewLine
